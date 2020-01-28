79544eb92d1c7842062157667c40e6bfc1266d68
Ubuntu <ubuntu@ip-172-31-19-42.us-east-2.compute.internal>
2020-01-28 10:20:56 +0000

    dfa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Parin
+                    Promact
                 </div>
 
                 <div class="links">
